d5d27b655ac1818e7847569d6b976a8ce5b997a7
Ryan0621 <ryan07063@gmail.com>
2018-12-30 16:06:34 -0800

    Add multiple selection choice

--- src/Second.js
+++ src/Second.js
@@ -1,11 +1,53 @@
 import React, { Component } from 'react';
 import {
-  Form, Select, Input, Button, Checkbox, Row, Col,
+  Form, Select, Button, Checkbox, Row, Col,
 } from 'antd';
-
+import axios from 'axios';
 const { Option } = Select;
 
 class Second extends Component {
+
+  constructor(props) {
+      super(props);
+
+      this.initialState = {
+          cuisine_type: [],
+          outlet_type: [],
+          affordability_type: [],
+
+          cuisine_choice: [],
+          outlet_choice: [],
+          affordability_choice: [],
+          halal_choice: false,
+          vegan_choice: false,
+      };
+
+      this.state = this.initialState;
+  }
+
+  componentDidMount() {
+    axios.get(`http://127.0.0.1:8000/cuisine_type/`)
+      .then(res => {
+        if(res.status === 200){
+            this.setState({cuisine_type: [...this.state.cuisine_type, ...res.data]});
+        }
+      })
+
+    axios.get(`http://127.0.0.1:8000/outlet_type/`)
+      .then(res => {
+        if(res.status === 200){
+            this.setState({outlet_type: [...this.state.outlet_type, ...res.data]});
+        }
+      })
+
+    axios.get(`http://127.0.0.1:8000/affordability/`)
+      .then(res => {
+        if(res.status === 200){
+            this.setState({affordability_type: [...this.state.affordability_type, ...res.data]});
+        }
+      })
+  }
+
   handleSubmit = (e) => {
     e.preventDefault();
     this.props.form.validateFields((err, values) => {
@@ -15,16 +57,41 @@ class Second extends Component {
     });
   }
 
-  handleSelectChange = (value) => {
-    console.log(value);
+  handleCuisineChange = (key, value) => {
+    const cuisine_choice = value.map(item => {
+      return({
+        'id': item.key,
+        'cuisine_type': item.props.value
+      })
+    })
+    this.setState({cuisine_choice: cuisine_choice})
+  }
 
+  handleOutletChange = (key, value) => {
+    const outlet_choice = value.map(item => {
+      return({
+        'id': item.key,
+        'outlet_type': item.props.value
+      })
+    })
+    this.setState({outlet_choice: outlet_choice})
+  }
+
+  handleAffordabilityChange = (key, value) => {
+    const affordability_choice = value.map(item => {
+      return({
+        'id': item.key,
+        'cuisine_type': item.props.value
+      })
+    })
+    this.setState({affordability_choice: affordability_choice})
   }
 
   render() {
-    const { getFieldDecorator } = this.props.form;
-    return (
-      <Form onSubmit={this.handleSubmit}>
 
+    return (
+
+      <Form onSubmit={this.handleSubmit}>
         <Form.Item
           label="Cuisine Type"
           labelCol={{ span: 5 }}
@@ -32,11 +99,15 @@ class Second extends Component {
         >
 
         <Select
-          placeholder="No preferences"
-          onChange={this.handleSelectChange}
+          mode="multiple"
+          style={{ width: '100%' }}
+          placeholder="Please select"
+          defaultValue={[]}
+          onChange={this.handleCuisineChange}
         >
-          <Option value="Chinese">Chinese</Option>
-          <Option value="Mexican">Mexican</Option>
+
+        {this.state.cuisine_type.map(item => (<Option key={item.id} value={item.cuisine_type}>{item.cuisine_type}</Option>))}
+
         </Select>
 
         </Form.Item>
@@ -48,11 +119,15 @@ class Second extends Component {
         >
 
         <Select
-          placeholder="No preferences"
-          onChange={this.handleSelectChange}
+          mode="multiple"
+          style={{ width: '100%' }}
+          placeholder="Please select"
+          defaultValue={[]}
+          onChange={this.handleOutletChange}
         >
-          <Option value="Restaurant">Restaurant</Option>
-          <Option value="Dessert">Dessert</Option>
+
+        {this.state.outlet_type.map(item => (<Option key={item.id} value={item.outlet_type}>{item.outlet_type}</Option>))}
+
         </Select>
 
         </Form.Item>
@@ -64,11 +139,15 @@ class Second extends Component {
         >
 
         <Select
-          placeholder="No preferences"
-          onChange={this.handleSelectChange}
+          mode="multiple"
+          style={{ width: '100%' }}
+          placeholder="Please select"
+          defaultValue={[]}
+          onChange={this.handleAffordabilityChange}
         >
-          <Option value="$">$</Option>
-          <Option value="$$">$$</Option>
+
+        {this.state.affordability_type.map(item => (<Option key={item.id} value={item.affordability}>{item.affordability}</Option>))}
+
         </Select>
 
         </Form.Item>
@@ -80,7 +159,7 @@ class Second extends Component {
             <Col span={8}><Checkbox value="Halal">Halal</Checkbox></Col>
             <Col span={8}><Checkbox value="Vegan-friendly">Vegan-friendly</Checkbox></Col>
           </Row>
-        </Checkbox.Group>,
+        </Checkbox.Group>
 
         </Form.Item>
 
