5a3010acb48d4b363146640a5c5f943e6b9af301
Ryan0621 <ryan07063@gmail.com>
2019-02-09 15:35:16 -0800

    add build

---
+++ build/precache-manifest.f387660048f46685727efe92643c714d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c3aedce796e19637e866",
+    "url": "/static/css/main.302a2679.chunk.css"
+  },
+  {
+    "revision": "c3aedce796e19637e866",
+    "url": "/static/js/main.c3aedce7.chunk.js"
+  },
+  {
+    "revision": "0c603face28d7912b2da",
+    "url": "/static/css/1.4c2a5904.chunk.css"
+  },
+  {
+    "revision": "0c603face28d7912b2da",
+    "url": "/static/js/1.0c603fac.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "94bec7bc9dde05ef5488cd8696d55d33",
+    "url": "/index.html"
+  }
+];
